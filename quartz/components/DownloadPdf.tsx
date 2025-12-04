1799e860ea03f524d52cdd1219ce5abc91434c3d
Francesco Di Betta <francescodibetta@MacBookAir.homenet.telecomitalia.it>
2025-12-05 00:13:52 +0100

    Quartz sync: Dec 5, 2025, 12:13 AM

--- quartz/components/DownloadPdf.tsx
+++ quartz/components/DownloadPdf.tsx
@@ -51,25 +51,26 @@ export default (() => {
     }
 
     // 3. Render the Button
-  return (
-  <div class={classNames(displayClass, "pdf-container")}>
-    {/* ADDED "/notebook" prefix here! */}
-    <a href={`/notebook/static/pdfs/${pdfFile}`} download class="pdf-download-btn" title="Download PDF version">
-      {/* SVG Icon: Download */}
-      <svg 
-        xmlns="http://www.w3.org/2000/svg" 
-        width="24" height="24" viewBox="0 0 24 24" 
-        fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-        class="pdf-icon"
-      >
-        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-        <polyline points="7 10 12 15 17 10"></polyline>
-        <line x1="12" y1="15" x2="12" y2="3"></line>
-      </svg>
-      Download PDF
-    </a>
-  </div>
-)
+    return (
+      <div class={classNames(displayClass, "pdf-container")}>
+        {/* ADDED "/notebook" prefix here! */}
+        <a href={`/notebook/static/pdfs/${pdfFile}`} download class="pdf-download-btn" title="Download PDF version">
+          {/* SVG Icon: Download */}
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            width="24" height="24" viewBox="0 0 24 24" 
+            fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+            class="pdf-icon"
+          >
+            <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+            <polyline points="7 10 12 15 17 10"></polyline>
+            <line x1="12" y1="15" x2="12" y2="3"></line>
+          </svg>
+          Download PDF
+        </a>
+      </div>
+    )
+  } // <--- THIS BRACE WAS MISSING
 
   DownloadPdf.css = style
   return DownloadPdf
